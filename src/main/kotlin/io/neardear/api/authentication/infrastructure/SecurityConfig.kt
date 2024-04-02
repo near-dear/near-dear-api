7673feea0d7530975cd26926c837dd11d56f4c9e
Wanni <wannidev0928@gmail.com>
2024-04-02 17:38:01 +0900

    Feat: 시연하기 쉽게 스프링 시큐리티 시연용으로 임시로 유연하게 적용

---
+++ src/main/kotlin/io/neardear/api/authentication/infrastructure/SecurityConfig.kt
@@ -0,0 +1,34 @@
+package io.neardear.api.authentication.infrastructure
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors { it.disable() }
+            .sessionManagement { it.disable() }
+            .authorizeHttpRequests { configurer ->
+                configurer.anyRequest().permitAll() // TODO mock api 작성 이후에 api endpoint 인가 설정 적용하기
+            }
+            .formLogin { it.disable() }
+            .build()
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web ->
+            web.ignoring().requestMatchers(PathRequest.toH2Console())
+        }
+    }
+}

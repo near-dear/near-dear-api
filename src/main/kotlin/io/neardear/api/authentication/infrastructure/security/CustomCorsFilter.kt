b9825dae9aef850cd66a86d0a4f2ba64ed027ed0
Wanni <wannidev0928@gmail.com>
2024-04-10 15:50:43 +0900

    Refactor: origin 헤더 수정

--- src/main/kotlin/io/neardear/api/authentication/infrastructure/security/CustomCorsFilter.kt
+++ src/main/kotlin/io/neardear/api/authentication/infrastructure/security/CustomCorsFilter.kt
@@ -10,7 +10,7 @@ class CustomCorsFilter : CorsFilter(configurationSource()) {
         private fun configurationSource(): UrlBasedCorsConfigurationSource {
             val config = CorsConfiguration()
             config.allowCredentials = true
-            config.allowedOriginPatterns = listOf("*")
+            config.allowedOriginPatterns = listOf("http://localhost:3000")
             config.addAllowedHeader("*")
             config.maxAge = 36000L
             config.allowedMethods = listOf("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
@@ -18,7 +18,9 @@ class CustomCorsFilter : CorsFilter(configurationSource()) {
             source.registerCorsConfiguration("/**", config)
             source.registerCorsConfiguration("/v1/**", config)
             source.registerCorsConfiguration("/v1/auth/**", config)
+            source.registerCorsConfiguration("/v1/user/**", config)
             source.registerCorsConfiguration("/v1/product/**", config)
+            source.registerCorsConfiguration("/v1/merchant/**", config)
             return source
         }
     }
